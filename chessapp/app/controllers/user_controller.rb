9dc11f584eb16dcd299bc1375719c5c24d2d9a6c
nick <a6ashin@gmail.com>
2018-12-29 22:49:45 +0300

    29.12.18

--- chessapp/app/controllers/user_controller.rb
+++ chessapp/app/controllers/user_controller.rb
@@ -1,17 +1,38 @@
 class UserController < ApplicationController
   before_action :set_champions
+
   def game
   end
+
   def main_menu
   end
+
   def set_champions
     @champions ||= %w(Алехин Ботвинник Смыслов Таль)
     @enemy = @champions.sample
     @champions = @champions.delete_if {|x| x== @enemy}
   end
+
   def champions
   end
-  def show
-    @user = Player.find(params[:id])
+
+  def new_player
+
   end
+
+  def create_player
+
+  end
+
+  def show
+    @player = Player.find_by(id: params[:id])
+    if @player.nil?
+      redirect_to root_path
+    end
+  end
+
+  def set_players
+    @players = Player.all
+  end
+
 end
--- chessapp/config/routes.rb
+++ chessapp/config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   get '/game', to: 'user#game'
   get '/champions', to: 'user#champions'
   get "/player/:id", to: "user#show", as: 'player'
+  get '/create_player', to: "user#create_player"
+  get '/new_player', to: "user#new_player"
+  post '/create_player', to: "user#create_player"
   root 'user#main_menu'
 end
